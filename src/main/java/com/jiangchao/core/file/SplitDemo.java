7ea2f596c22b0756378d024fb2e4fc84736ca954
jiangchao1987 <jiangchao1987@gmail.com>
2013-05-17 15:05:19 +0800

    split demo.
    Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

---
+++ src/main/java/com/jiangchao/core/file/SplitDemo.java
@@ -0,0 +1,13 @@
+package com.jiangchao.core.file;
+
+public class SplitDemo {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
